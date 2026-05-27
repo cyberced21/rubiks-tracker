af2807d41cd74b023493c66fc91ed02638c63682
Cedrik Blais <cblais@cyberdefense.ai>
2026-05-27 12:39:05 -0400

    updated dependencies

---
+++ electron.vite.config.1779899733196.mjs
@@ -0,0 +1,29 @@
+// electron.vite.config.ts
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+var electron_vite_config_default = defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        "@renderer": resolve("src/renderer/src")
+      }
+    },
+    plugins: [react()],
+    optimizeDeps: {
+      exclude: ["cubing"]
+    },
+    worker: {
+      format: "es"
+    }
+  }
+});
+export {
+  electron_vite_config_default as default
+};
